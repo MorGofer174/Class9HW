15a27981a8e4673be85d7e1301b5dee15cfcf79e
MorGofer174 <morgofer174@gmail.com>
2021-06-28 12:35:28 +0300

    update

--- src/test/java/TestNG.java
+++ src/test/java/TestNG.java
@@ -7,26 +7,24 @@ import org.testng.annotations.Test;
 public class TestNG {
         private static WebDriver chromeDriver;
         private static WebDriver fireFoxDriver;
+        private static String websiteName;
 
         @BeforeClass
         public static void runsBeforeClass() {
             System.setProperty("webdriver.chrome.driver", "C:\\Users\\MorG\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
             chromeDriver = new ChromeDriver();
             chromeDriver.manage().window().maximize();
-            chromeDriver.get("https://translate.google.com");
+            chromeDriver.get("https://www.walla.co.il");
 
             System.setProperty("webdriver.gecko.driver","C:\\Users\\MorG\\Downloads\\geckodriver-v0.29.1-win64\\geckodriver.exe");
             fireFoxDriver = new FirefoxDriver();
             fireFoxDriver.manage().window().maximize();
-            fireFoxDriver.get("https://translate.google.com");
+            fireFoxDriver.get("https://www.ynet.co.il");
         }
 
-        @Test
-        public void opensWalla(){
-            chromeDriver.get("https://www.walla.co.il");
+        @Test (priority=1)
+        public void opensWalla() {
+            websiteName = chromeDriver.getTitle();
+            System.out.println(websiteName);
         }
-        @Test
-        public void opens(){
-            fireFoxDriver.get("https://www.ynet.co.il");
-    }
 }
